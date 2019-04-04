aeb870a44b7ae906b469a3227376abbce172b279
Carolinepandolfe <carolinepandolfe@yahoo.com.br>
2019-04-04 13:18:16 -0300

    Alterar back-end para django

--- API/models/pokemons.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-const PokemonSchema = new mongoose.Schema({
-
-});
---
+++ crud_pokemon/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CrudPokemonConfig(AppConfig):
+    name = 'crud_pokemon'
